d3a12d66a28c6e388ef86d15702563a6933465e4
Georgiy Krylov <georgiykylov@gmail.com>
2015-04-10 02:53:05 +0600

    Some additions to pauseplay

--- OOADsema/src/PausePlayClass.java
+++ OOADsema/src/PausePlayClass.java
@@ -1,4 +1,13 @@
+import javax.swing.SwingWorker;
 
-public class PausePlayClass {
 
+public class PausePlayClass extends SwingWorker<Void,Void> {{
+
+}
+
+@Override
+protected Void doInBackground() throws Exception {
+	// TODO Auto-generated method stub
+	return null;
+}
 }
